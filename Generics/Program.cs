6432e297ef8dfc20095f4e4d40a594af15dd1f8f
Serkan IŞIK <dehnas79@gmail.com>
2023-06-07 14:48:20 +0300

    Add Generics

---
+++ Generics/Program.cs
@@ -0,0 +1,53 @@
+﻿
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> cities = new List<string>();
+            cities.Add("London");
+            cities.Add("Paris");
+            cities.Add("Milan");
+            cities.Add("New York");
+            cities.Add("Tokyo");
+            Console.WriteLine(cities.Count);
+
+
+            MyList<string> cities2 = new MyList<string>();
+            cities2.Add("London");
+            cities2.Add("Paris");
+            cities2.Add("Milan");
+            cities2.Add("New York");
+            cities2.Add("Tokyo");
+            Console.WriteLine(cities2.Count);
+
+
+            Console.ReadKey();
+        }
+    }
+
+    class MyList<T> // Generic Class
+    {
+        T[] _array;
+        public MyList()
+        {
+            _array = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = _array;
+            _array = new T[_array.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                _array[i] = tempArray[i];
+            }
+            _array[_array.Length - 1] = item;
+        }
+
+        public int Count
+        {
+            get { return _array.Length; }
+        }
+    }
+}
