404651020945ace0da4581a6657085f378bc65a4
Serkan IŞIK <dehnas79@gmail.com>
2023-08-07 15:23:32 +0300

    Add Linq

---
+++ Linq/DataSource.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    public class DataSource
+    {
+        List<Customer> customers;
+
+        public DataSource()
+        {
+            customers = new List<Customer>();
+        }
+
+        public List<Customer> CustomerList()
+        {
+            for (int i = 1; i <= 1000; i++)
+            {
+                Customer tempCustomer = new Customer();
+                tempCustomer.CustomerNumber = i;
+                tempCustomer.FirstName = FakeData.NameData.GetFirstName();
+                tempCustomer.LastName = FakeData.NameData.GetSurname();
+                tempCustomer.DateOfBirth = FakeData.DateTimeData.GetDatetime();
+
+                customers.Add(tempCustomer);
+            }
+            return customers;
+        }
+    }
+}
---
+++ Linq/Program.cs
@@ -0,0 +1,29 @@
+﻿using Linq; // This is the namespace that contains the Customer class
+
+DataSource dataSource = new DataSource();
+
+List<Customer> customerList = dataSource.CustomerList();
+Console.WriteLine(customerList.Count);
+Console.WriteLine("*********************************************************************");
+
+//Number of customers whose name starts with A
+int counter = 0;
+
+for (int i = 0; i < customerList.Count; i++)
+{
+    if (customerList[i].FirstName[0] == 'A')
+    {
+        counter++;
+    }
+}
+
+Console.WriteLine("Number of customers whose name starts with A in the list: {0}",counter);
+Console.WriteLine("*********************************************************************");
+
+int output = 0;
+
+output = customerList.Where(x => x.FirstName.StartsWith("A")).Count();
+
+Console.WriteLine("Number of customers whose name starts with A in the list: {0}", output);
+
+Console.ReadLine();
