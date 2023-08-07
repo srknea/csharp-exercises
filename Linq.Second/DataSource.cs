a91f6400e54377a40e5344b50bf3dbe7fbd33891
Serkan IŞIK <dehnas79@gmail.com>
2023-08-07 17:26:11 +0300

    Add Linq.Second

---
+++ Linq.Second/DataSource.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq.Second
+{
+    public class DataSource
+    {
+        List<Customer> customers;
+
+        public DataSource()
+        {
+            customers = new List<Customer>();
+        }
+
+        public List<Customer> CustomerList()
+        {
+            for (int i = 1; i <= 1000; i++)
+            {
+                Customer tempCustomer = new Customer();
+                tempCustomer.CustomerNumber = i;
+                tempCustomer.FirstName = FakeData.NameData.GetFirstName();
+                tempCustomer.LastName = FakeData.NameData.GetSurname();
+                tempCustomer.DateOfBirth = FakeData.DateTimeData.GetDatetime(new DateTime (1984, 01, 01), new DateTime (1999, 01, 01));
+                tempCustomer.Country = FakeData.PlaceData.GetCountry();
+                tempCustomer.City = FakeData.PlaceData.GetCity();
+                
+                tempCustomer.Email = $"{tempCustomer.FirstName}.{tempCustomer.LastName}@{FakeData.NetworkData.GetDomain()}";
+                
+                tempCustomer.PhoneNumber = FakeData.PhoneNumberData.GetPhoneNumber();
+
+                customers.Add(tempCustomer);
+            }
+            return customers;
+        }
+    }
+}
---
+++ Linq.Second/Program.cs
@@ -0,0 +1,22 @@
+﻿using Linq.Second; // This is the namespace that contains the Customer class
+
+DataSource dataSource = new DataSource();
+
+List<Customer> customerList = dataSource.CustomerList();
+
+
+int output = 0;
+
+output = customerList.Where(I => I.FirstName.StartsWith("A")).Count();
+
+Console.WriteLine(output);
+
+int secoundOutput = 0;
+
+secoundOutput = (from I in customerList
+                 where I.FirstName.StartsWith("A")
+                 select I).Count();
+
+Console.WriteLine(secoundOutput);
+
+Console.ReadLine();
